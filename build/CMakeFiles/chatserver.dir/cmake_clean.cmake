261d0a8dfc4290cde6ee4519ec91debf3683f62e
lollerfirst <lollerfirst@gmail.com>
2022-11-19 12:31:18 +0100

    Lorem Ipsum dolor sit amet

---
+++ build/CMakeFiles/chatserver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chatserver.dir/server.c.o"
+  "chatserver"
+  "chatserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chatserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
