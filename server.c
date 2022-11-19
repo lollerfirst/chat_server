fb19362f8afb38fcce7dd8b63404c9bb87742ab7
lollerfirst <lollerfirst@gmail.com>
2022-11-19 08:16:03 +0100

    Lorem Ipsum Dolor Sit Amet

--- server.c
+++ server.c
@@ -1,10 +1,3 @@
-/* TODO LIST:
-	-add a enum for error enumeration
-	-add conn_handler
-	-add quit_handler
-
-*/
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,192 +10,192 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <signal.h>
-#include "queue.h"
-#include "vector.h"
+#include <bool.h>
+#include <errno.h>
+#include <assert.h>
+
 #define PORT 1800
 #define MAX_CLIENTS 20
-#define MAX_LINE 1024
+#define MAX_LINE 512
 #define SERVER_BACKLOG 100
 
-pthread_mutex_t mutex;
-pthread_cond_t cond_var;
-pthread_t pool[MAX_CLIENTS];
-pthread_t mex_thread;
-pthread_cond_t mex_cond_var;
-Elem connected_clients[MAX_CLIENTS];
-size_t conn_cli_len;
+//TYPE DEFINITIONS
 
-char message[MAX_LINE+1];
-Elem last_cli;
+typedef struct __Client{
+	struct sockaddr_in6 cli_addr;
+	int cli_fd;
+	bool busy;
+	pthread_t thread;
+	pthread_mutex_t lock;
+} Client;
 
-//Close everything on SIGINT signal
-void quit_handler(int c){
+//GLOBAL VARIABLES
+
+static Client clients[MAX_CLIENTS];
+
+void quit_handler(int c)
+{
 	
-	//Cancel all the threads
-	int i;
-	for(i=0; i<MAX_CLIENTS; i++)
-		pthread_cancel(pool[i]);
-	
-	pthread_cancel(mex_thread);
-	
-	//Close all connections
-	for(i=0; i<conn_cli_len; i++)
-		close(connected_clients[i].cli_fd);
-	
-	//Deinit the pending connection queue
-	queue_deinit();
-	
-	//Destroy mutexes and condition variables
-	pthread_mutex_destroy(&mutex);
-	pthread_cond_destroy(&cond_var);
-	pthread_cond_destroy(&mex_cond_var);
-	
-	//Exit the process with given value
-	exit(c);
 }
 
-void* conn_handler(void* arg){
-	Elem el;
-	char buffer[MAX_LINE+1];
-	char addr[INET6_ADDRSTRLEN];
-	
-	while(1){
-		bzero(&el, sizeof(Elem));
-		bzero(buffer, sizeof(buffer));
-		bzero(addr, sizeof(addr));
-		pthread_mutex_lock(&mutex);
-		
-		//Check if there is an element on the queue. If there is not wait for the signal
-		if(dequeue(&el) < 0){
-			pthread_cond_wait(&cond_var, &mutex);
-			dequeue(&el);	
+int broadcast(const char *message, size_t size)
+{
+	int i;
+	for (i=0; i<MAX_CLIENTS; ++i){
+		pthread_mutex_lock(&clients[i].lock);
+
+		if (clients[i].busy)
+		{
+			if (write(clients[i].cli_fd, message, size) < 0)
+			{
+				perror(strerror(errno));
+				pthread_mutex_unlock(&clients[i].lock);
+				return -1;
+			}
+
 		}
-		vector_push(connected_clients, &el, &conn_cli_len, sizeof(Elem));
-		inet_ntop(AF_INET6, &el.cli_addr.sin6_addr.in6_addr.s6_addr, addr, INET6_ADDRSTRLEN);
-		printf("[Thread %d] accepted 1 connection: %s\n", (int) pthread_self(), addr);
-		pthread_mutex_unlock(&mutex);
-		
-		while(read(el.cli_fd, buffer, sizeof(buffer)) > 0){
-			pthread_mutex_lock(&mutex);
-			last_cli = el;
-			memcpy(message, buffer, sizeof(buffer));
-			pthread_mutex_unlock(&mutex);
-			
-			pthread_cond_signal(&mex_cond_var);
-		} 
-		
-		//Client has disconnected or there has been an error.
-		close(el.cli_fd);
-		pthread_mutex_lock(&mutex);
-		vector_remove(connected_clients, &el, &conn_cli_len, sizeof(Elem));
-		pthread_mutex_unlock(&mutex);
-		
-		return NULL;
+
+		pthread_mutex_unlock(&clients[i].lock);
 	}
+
+	return 0;
 }
 
-//Once notified that there is a new message, it takes it and re-broadcasts it to all clients.
-void* mex_handler(void* arg){
-	char buffer[MAX_LINE+1];
-	size_t buf_len = sizeof(buffer);
-	int i;
-	
-	
-	while(1){
-		pthread_mutex_lock(&mutex);
-		pthread_cond_wait(&mex_cond_var, &mutex);
-		inet_ntop(AF_INET6, &last_cli.cli_addr.sin6_addr.in6_addr, buffer, buf_len); 
-		strcat(buffer, ": ");
-		strcat(buffer, message);
-		strcat(buffer, '\0');
-		
-		
-		for(i=0; i<conn_cli_len; i++)
-			write(connected_clients[i].cli_fd, buffer, sizeof(buffer));
-		
-		pthread_mutex_unlock(&mutex);
-		bzero(buffer, sizeof(buffer));
-		
-		return NULL;
+void* conn_routine(void* arg)
+{
+
+	// Sanity check
+	assert(arg != NULL);
+
+	// Install the signal handlers
+	struct sigaction sigact = {0};
+	sigact.sa_handler = quit_handler;
+	sigaction(SIGUSR1, &sigact, NULL);
+	sigaction(SIGINT, &sigact, NULL);
+
+	// Vars
+	size_t iterator = *(size_t*) arg;
+	ssize_t bytes;
+	char buffer[MAX_LINE + 1] = {0};
+	char from[64] = {0};
+	inet_ntop(AF_INET6, &clients[iterator].cli_addr, from, sizeof(from));
+
+	// only this thread can READ from cli_fd
+	while ((bytes = read(clients[iterator].cli_fd, buffer, MAX_LINE)) > 0)
+	{
+		//Append sender address to message
+		char message[1024] = {0};
+		sprintf(message, "<%s>: %s", from, buffer);
+
+		//Broadcast message to other clients
+		(void) broadcast(message, sizeof(message));
 	}
+
+	if (bytes == -1)
+	{
+		perror(strerror(errno));
+	}
+
+	// Other threads may be trying to write to cli_fd
+	pthread_mutex_lock(&clients[iterator].lock);
+
+	clients[iterator].busy = false;
+	close(clients[iterator].cli_fd);
+
+	pthread_mutex_unlock(&clients[iterator].lock);
 }
 
-int main(int argc, char** argv){
-	struct sockaddr_in6 address, cli_addr;
+int main()
+{
+	struct sockaddr_in6 address;
 	size_t addrlen = sizeof(address);
+	size_t iterator = 0;
 	int listen_fd;
-	int iter;
-	bzero(&addr, sizeof(struct sockaddr_in));
-	bzero(&cli_addr, sizeof(struct sockaddr_in));
-	bzero(&connected_clients, sizeof(connected_clients));
-	conn_cli_len = 0;
-	
-	//Setting up the SIGINT handler
-	signal(SIGINT, &quit_handler);
-	
-	//Set the address
+
+	// INITIALIZING STUFF
+	bzero(&address, sizeof(struct sockaddr_in6));
+	bzero(clients, sizeof(clients));
+
+	size_t k;
+	for (k=0; k<MAX_CLIENTS; ++k){
+		clients[k].lock = PTHREAD_MUTEX_INITIALIZER;
+	}
+
+	// SET UP CONNECTION TYPE IPv6, HOST IP, PORT
 	address.sin6_family = AF_INET6;
 	address.sin6_addr.in6_addr.s6_addr = htonl(INADDRESS_ANY);
 	address.sin6_port = htons(PORT);
-	
-	//Create the socket
-	if((listen_fd = socket(AF_INET6, SOCK_STREAM, 0)) == -1){
+
+	// CREATE THE SOCKET
+	if ((listen_fd = socket(AF_INET6, SOCK_STREAM, 0)) == -1)
+	{
 		perror(strerror(errno));
 		exit(errno);
 	}
-	
-	//Bind the socket
-	if(bind(listen_fd, (struct sockaddr*) &address, addrlen) == -1){
-		perror(strerror(errno));
-		close(listen_fd);
-		exit(errno);
-	}
-	
-	//Mark the socket as passive
-	if(listen(listen_fd, SERVER_BACKLOG) == -1){
+
+	// Bind the socket
+	if (bind(listen_fd, (struct sockaddr *)&address, addrlen) == -1)
+	{
 		perror(strerror(errno));
 		close(listen_fd);
 		exit(errno);
 	}
-	
-	//Initialize the threads and threads vars
-	pthread_mutex_init(&mutex);
-	pthread_cond_init(&cond_var);
-	for(iter = 0; iter < MAX_CLIENTS; iter++)
-		pthread_create(pool+iter, NULL, &conn_handler, NULL);
-	
-	pthread_cond_init(&mex_cond_var);
-	pthread_create(&mex_thread, NULL, &mex_handler, NULL);
-	
-	//Initialize the pending connections queue
-	if(queue_init(SERVER_BACKLOG) < 0){
-		perror("Queue couldn't initialize!\n");
+
+	// Mark the socket as passive
+	if (listen(listen_fd, SERVER_BACKLOG) == -1)
+	{
+		perror(strerror(errno));
 		close(listen_fd);
-		sigint_handler(-1);
+		exit(errno);
 	}
-	
-	//Wait for connections and enqueue them
-	while(1){
-		static struct sockaddr tmp;
-		static Elem el;
-		size_t cli_addr_len = sizeof(struct sockaddr);
-		static int fd;
-		if((fd = accept(listen_fd, &tmp, &cli_addr_len, 0)) < 0){
+
+	// Listen for and enqueue connections
+	while (1)
+	{
+		struct sockaddr_in6 cli_addr = {0};
+		socklen_t addrlen = sizeof(struct sockaddr_in6);
+
+		int cli_fd;
+		if ((cli_fd = accept(listen_fd, (struct sockaddr *)&cli_addr, &addrlen)) == -1)
+		{
 			perror(strerror(errno));
-			sigint_handler(errno);
+			kill(0, SIGUSR1);
+			close(listen_fd);
+
+			exit(errno);
 		}
-		el.cli_addr = tmp.sockaddr_in6;
-		el.cli_fd = fd;
-		
-		pthread_mutex_lock(&mutex);
-		printf("1 connection queued\n");
-		enqueue(el);
-		pthread_mutex_unlock(&mutex);
-		phtread_cond_signal(&cond_var); //signal that a connection has just been appeneded to the queue;
-		
-		bzero(&tmp, sizeof(struct sockaddr));
-		bzero(&el, sizeof(Elem));
+
+		// find an available slot; only main thread can set busy to true
+		size_t i;
+		for (i=0; (clients[iterator].busy) && (i < MAX_CLIENTS); ++i)
+			iterator = (iterator + 1) % MAX_CLIENTS;
+
+		if (i < MAX_CLIENTS)
+		{
+
+			pthread_mutex_lock(&clients[iterator].lock);
+
+			// append the client file descriptor to the queue and pass it to the thread
+			clients[iterator].cli_addr = cli_addr;
+			clients[iterator].cli_fd = cli_fd;
+			clients[iterator].busy = true;
+
+			//thread stack is independent from main process so iterator is copied into it presumably.
+			if ((pthread_create(&clients[iterator].thread, NULL, conn_routine, (void*) &iterator) != 0))
+			{
+				clients[iterator].busy = false;
+				perror(strerror(errno));
+				close(cli_fd);
+			}
+
+			pthread_mutex_unlock(&clients[iterator].lock);
+		}
+		else
+		{
+			close(cli_fd);
+		}
 	}
-	
+
 	return 0;
 }
+
--- vector.h
+++
@@ -1,7 +0,0 @@
-#ifndef __VECTOR_H_
-#define __VECTOR_H_ 1
-
-extern void vector_push(void* vector, const void*  element, size_t vector_len, size_t element_size);
-extern void vector_remove(void* vector, const void*  element, size_t vector_len, size_t element_size);
-
-#endif
